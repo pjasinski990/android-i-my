2582646f574ea11f74de68d69b66206a55c46a44
Piotr Jasinski <piotr.jasinski990@gmail.com>
2019-11-26 23:33:54 +0100

    don't let me down

--- app/src/main/java/com/example/bloodpurification/screens/input/InputFragment.kt
+++ app/src/main/java/com/example/bloodpurification/screens/input/InputFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.bloodpurification.R
 import com.example.bloodpurification.databinding.FragmentInputBinding
+
+private lateinit var viewModel : InputFragmentViewModel
 
 class InputFragment : Fragment() {
     override fun onCreateView(
@@ -16,14 +19,18 @@ class InputFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val binding = DataBindingUtil.inflate<FragmentInputBinding>(inflater,
             R.layout.fragment_input, container,false)
+
+        viewModel = ViewModelProviders.of(this).get(InputFragmentViewModel::class.java)
+
 
         val navController = findNavController()
         binding.inputButton.setOnClickListener {
             navController.navigate(R.id.action_input_to_simulation)
         }
+
+
 
         return binding.root
     }
--- app/src/main/java/com/example/bloodpurification/screens/input/InputFragmentViewModel.kt
+++ app/src/main/java/com/example/bloodpurification/screens/input/InputFragmentViewModel.kt
@@ -9,5 +9,32 @@ class InputFragmentViewModel : ViewModel(){
     val cEnd = MutableLiveData<Double>()
     val vTotal = MutableLiveData<Double>()
     val clearanceInter = MutableLiveData<Double>()
+    val genRate = MutableLiveData<Double>()
+    val clearanceAvg = MutableLiveData<Double>()
+    val tTreatment= MutableLiveData<Double>()
 
+    fun updateCPre(newValue : Double) {
+        cPre.value = newValue
+    }
+    fun updateCPost(newValue : Double) {
+        cPost.value = newValue
+    }
+    fun updateCEnd(newValue : Double) {
+        cEnd.value = newValue
+    }
+    fun updateVTotal(newValue : Double) {
+        vTotal.value = newValue
+    }
+    fun updateClearanceInter(newValue : Double) {
+        clearanceInter.value = newValue
+    }
+    fun updateGenRate(newValue : Double) {
+        genRate.value = newValue
+    }
+    fun updateClearanceAvg(newValue : Double) {
+        clearanceAvg.value = newValue
+    }
+    fun updateTTreatment(newValue : Double) {
+        tTreatment.value = newValue
+    }
 }
