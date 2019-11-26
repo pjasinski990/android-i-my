06919dc5ffa52e8c03a3cc05c53b5864b9a21d61
Piotr Jasinski <piotr.jasinski990@gmail.com>
2019-11-26 22:32:17 +0100

    refactor (screens package)

--- app/src/main/java/com/example/bloodpurification/screens/simulation/Simulation.kt
+++ app/src/main/java/com/example/bloodpurification/screens/simulation/Simulation.kt
@@ -1,4 +1,4 @@
-package com.example.bloodpurification
+package com.example.bloodpurification.screens.simulation
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.bloodpurification.R
 import com.example.bloodpurification.databinding.FragmentGraphBinding
 
 class Simulation : Fragment() {
--- app/src/main/java/com/example/bloodpurification/screens/start/Start.kt
+++ app/src/main/java/com/example/bloodpurification/screens/start/Start.kt
@@ -1,4 +1,4 @@
-package com.example.bloodpurification
+package com.example.bloodpurification.screens.start
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,21 +7,22 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.bloodpurification.databinding.FragmentInputBinding
+import com.example.bloodpurification.R
+import com.example.bloodpurification.databinding.FragmentStartBinding
 
-class Input : Fragment() {
+class Start : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = DataBindingUtil.inflate<FragmentInputBinding>(inflater,
-            R.layout.fragment_input, container,false)
+        val binding = DataBindingUtil.inflate<FragmentStartBinding>(inflater,
+            R.layout.fragment_start, container,false)
 
         val navController = findNavController()
-        binding.inputButton.setOnClickListener {
-            navController.navigate(R.id.action_input_to_simulation)
+        binding.startButton.setOnClickListener{
+            navController.navigate(R.id.action_start_to_input)
         }
 
         return binding.root
