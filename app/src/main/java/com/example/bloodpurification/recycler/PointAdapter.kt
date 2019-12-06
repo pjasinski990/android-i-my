119770a0ea51d3718307923e10fff12de5433a97
Piotr Jasinski <piotr.jasinski990@gmail.com>
2019-12-06 17:22:31 +0100

    layout update idk

--- app/src/main/java/com/example/bloodpurification/recycler/PointAdapter.kt
+++ app/src/main/java/com/example/bloodpurification/recycler/PointAdapter.kt
@@ -3,34 +3,27 @@ package com.example.bloodpurification.recycler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bloodpurification.R
 
 class PointAdapter(val pointsList: List<Point>) : RecyclerView.Adapter<PointAdapter.PointViewHolder>(){
-    private val list = ArrayList<Point>()
-
-    fun PointAdapter() {
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PointViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.new_item, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.new_item, parent, false) as LinearLayout
         return PointViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return pointsList.size
     }
 
     override fun onBindViewHolder(holder: PointViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     class PointViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun PointViewHolder(itemView: View) {
-            val concentrationInput = itemView.findViewById<EditText>(R.id.concentrationInput)
-            val timeInput = itemView.findViewById<EditText>(R.id.timeInput)
         }
     }
 }
